b542f4174dbeef11b639f82dbeb0b7b5cb98c19b
lxm <lxm_812@163.com>
2019-01-09 17:38:37 +0800

    modify

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+int x = 10;
+int y = 20;
+
+int test_add(int a, int b)
+{
+	return (a+b);
+}
+
 int main(int argc, char *argv[])
 {
-	printf("Hello!\n");
+	int z = 0;
+	z = test_add(x, y);
+	printf("Hello! z=%d\n", z);
 	return 0;
 }
